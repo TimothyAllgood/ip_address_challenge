0691b2a212af5a318be910e298fc38e4fe19f58a
Timothy Allgood <tpaj0818@gmail.com>
2021-09-13 23:11:05 -0500

    added top 100 function, basic testing, added quicksort implemntation

---
+++ js/quicksort.js
@@ -0,0 +1,39 @@
+const partition = (arr, start, end) =>{
+
+    // Taking the last element as the pivot
+    const pivotValue = arr[end];
+
+    // All values to the left are less than this, to the right they are greater
+    let pivotIndex = start; 
+
+    for (let i = start; i < end; i++) {
+        if (arr[i].requestCount > pivotValue.requestCount) {
+        // Swapping elements
+        [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+        // Moving to next element
+        pivotIndex++;
+        }
+    }
+    
+    // Putting the pivot value in the middle
+    [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]] 
+
+    return pivotIndex;
+};
+
+
+const quickSort = (arr, start, end) => {
+    // Base case or terminating case
+    if (start >= end) {
+        return;
+    }
+    
+    // Returns pivotIndex
+    let index = partition(arr, start, end);
+    
+    // Recursively apply the same logic to the left and right subarrays
+    quickSort(arr, start, index - 1);
+    quickSort(arr, index + 1, end);
+}
+
+module.exports = {quickSort};
--- js/server.js
+++ js/server.js
@@ -1,3 +1,6 @@
+// Import Quicksort algo
+// const quickSort = require('./quicksort');
+
 // Create IP address array to hold all IP addresses
 let allIpAddresses = [{ ip: '1.1.1.1', requestCount: 2 }];
 
@@ -49,21 +52,49 @@ const requestHandled = (ipAddress) => {
 	// If the ip is found we increase the request count
 	if (ipFound) {
 		allIpAddresses[index].requestCount++;
-		console.log(allIpAddresses[index]);
+		// console.log(allIpAddresses[index]);
 	}
 
 	// Otherwise we add the ip address to an array and set the request count to our initial value of 1
 	else {
 		allIpAddresses.push({ ip: ipAddress, requestCount: 1 });
-		console.log(`New IP`);
+		// console.log(`New IP`);
 	}
 };
 
 // Test functions
-console.time('Request Function');
-requestHandled('1.1.1.1');
-console.timeEnd('Request Function');
-requestHandled('2.2.2.2');
+// console.time('Request Function');
+// requestHandled('1.1.1.1');
+// console.timeEnd('Request Function');
+// requestHandled('2.2.2.2');
 
 // Log All IP addresses
 // console.log(allIpAddresses);
+
+// Returns the top 100 ips sorted by most requests made
+const top100 = () => {
+	// Time complexity O(n log(n))
+	// Sorts our array, we compare one value to the next, if it is larger we shift the larger number in front of the smaller
+	allIpAddresses.sort((a, b) => (a.requestCount > b.requestCount ? -1 : 1));
+
+	// Quick Sort Version
+	// quickSort.quickSort(allIpAddresses, 0, allIpAddresses.length - 1);
+
+	// empty array to hold our top ip addresses
+	let top100 = [];
+
+	// Loop through our sorted array and push the top 100 ip addresses into our array
+	for (let i = 0; i <= 100; i++) {
+		top100.push(allIpAddresses[i]);
+	}
+
+	// Use Javascript's built-in splice method to shrink array to 100 items
+	// top100 = sorted.splice(0, 100);
+
+	return top100;
+};
+
+// Calls our function
+console.time('top');
+console.log(top100());
+console.timeEnd('top');
